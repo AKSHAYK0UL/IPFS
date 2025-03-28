8438183b77478d2584eedcdd508049aeb7e7e5f9
AKSHAYK0UL <akshaykoul999@gmail.com>
2025-03-28 18:22:33 +0530

    enable the auth middleware

--- route/route.go
+++ route/route.go
@@ -3,11 +3,12 @@ package route
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/koulipfs/controller"
+	"github.com/koulipfs/middleware"
 )
 
 func RouteTable() *gin.Engine {
 	route := gin.Default()
-	route.POST("/add-transaction", controller.AddTransactionController)
+	route.POST("/add-transaction", middleware.AuthHeaderMiddleware(), controller.AddTransactionController)
 	route.GET("/transactions", controller.GetTransactionController)
 	route.GET("/transaction/:id", controller.GetByIdTransactionController)
 	return route
