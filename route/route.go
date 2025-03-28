ee91d817465c1496a0b049e0dd2eb6332a1306a8
AKSHAYK0UL <akshaykoul999@gmail.com>
2025-03-28 17:47:11 +0530

    minor bug fixes

--- route/route.go
+++ route/route.go
@@ -3,12 +3,11 @@ package route
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/koulipfs/controller"
-	"github.com/koulipfs/middleware"
 )
 
 func RouteTable() *gin.Engine {
 	route := gin.Default()
-	route.POST("/add-transaction", middleware.AuthHeaderMiddleware(), controller.AddTransactionController)
+	route.POST("/add-transaction", controller.AddTransactionController)
 	route.GET("/transactions", controller.GetTransactionController)
 	route.GET("/transaction/:id", controller.GetByIdTransactionController)
 	return route
