7b1b82b1917955a13509302fbf35f748eedfdeb8
AKSHAYK0UL <akshaykoul999@gmail.com>
2025-03-29 12:00:42 +0530

    added the CORS middleware

--- route/route.go
+++ route/route.go
@@ -1,15 +1,30 @@
 package route
 
 import (
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+
 	"github.com/gin-gonic/gin"
 	"github.com/koulipfs/controller"
 	"github.com/koulipfs/middleware"
 )
 
 func RouteTable() *gin.Engine {
-	route := gin.Default()
-	route.POST("/add-transaction", middleware.AuthHeaderMiddleware(), controller.AddTransactionController)
-	route.GET("/transactions", controller.GetTransactionController)
-	route.GET("/transaction/:id", controller.GetByIdTransactionController)
-	return route
+	router := gin.Default()
+	//CORS middleware
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{http.MethodGet, http.MethodOptions},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	router.POST("/add-transaction", middleware.AuthHeaderMiddleware(), controller.AddTransactionController)
+	router.GET("/transactions", controller.GetTransactionController)
+	router.GET("/transaction/:id", controller.GetByIdTransactionController)
+	return router
 }
