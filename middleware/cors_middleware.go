1421a54d8c0102bd450f2f83574236b01473b80b
AKSHAYK0UL <akshaykoul999@gmail.com>
2025-04-02 12:48:23 +0530

    minor bug fixes

--- middleware/cors_middleware.go
+++ middleware/cors_middleware.go
@@ -1,25 +1,25 @@
 package middleware
 
-import (
-	"net/http"
-	"time"
+// import (
+// 	"net/http"
+// 	"time"
 
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-contrib/cors"
+// 	"github.com/gin-gonic/gin"
+// )
 
-func CORS() gin.HandlerFunc {
-	corsMiddleware := cors.New(cors.Config{
-		AllowOrigins:     []string{"https://zahiqibrahim.github.io"},
-		AllowMethods:     []string{http.MethodGet, http.MethodOptions},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	})
+// func CORS() gin.HandlerFunc {
+// 	corsMiddleware := cors.New(cors.Config{
+// 		AllowOrigins:     []string{"https://zahiqibrahim.github.io"},
+// 		AllowMethods:     []string{http.MethodGet, http.MethodOptions},
+// 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+// 		ExposeHeaders:    []string{"Content-Length"},
+// 		AllowCredentials: true,
+// 		MaxAge:           12 * time.Hour,
+// 	})
 
-	return func(ctx *gin.Context) {
-		corsMiddleware(ctx)
-		ctx.Next()
-	}
-}
+// 	return func(ctx *gin.Context) {
+// 		corsMiddleware(ctx)
+// 		ctx.Next()
+// 	}
+// }
--- route/route.go
+++ route/route.go
@@ -1,6 +1,11 @@
 package route
 
 import (
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+
 	"github.com/gin-gonic/gin"
 	"github.com/koulipfs/controller"
 	"github.com/koulipfs/middleware"
@@ -8,8 +13,15 @@ import (
 
 func RouteTable() *gin.Engine {
 	router := gin.Default()
-	// middleware's
-	router.Use(middleware.RateLimiter(), middleware.CORS())
+	//CORS middleware
+	router.Use(middleware.RateLimiter(), cors.New(cors.Config{
+		AllowOrigins:     []string{"https://zahiqibrahim.github.io"},
+		AllowMethods:     []string{http.MethodGet, http.MethodOptions},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	router.POST("/add-transaction", middleware.AuthHeaderMiddleware(), controller.AddTransactionController)
 	router.GET("/transactions", controller.GetTransactionController)
