0f723638754835825356de9e92c6bffc35f2f69c
AKSHAYK0UL <akshaykoul999@gmail.com>
2025-04-03 12:39:45 +0530

    code refactoring

--- route/route.go
+++ route/route.go
@@ -1,11 +1,6 @@
 package route
 
 import (
-	"net/http"
-	"time"
-
-	"github.com/gin-contrib/cors"
-
 	"github.com/gin-gonic/gin"
 	"github.com/koulipfs/controller"
 	"github.com/koulipfs/middleware"
@@ -14,14 +9,16 @@ import (
 func RouteTable() *gin.Engine {
 	router := gin.Default()
 	//CORS middleware
-	router.Use(middleware.RateLimiter(), cors.New(cors.Config{
-		AllowOrigins:     []string{"https://zahiqibrahim.github.io"},
-		AllowMethods:     []string{http.MethodGet, http.MethodOptions},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
+	// router.Use(middleware.RateLimiter(), cors.New(cors.Config{
+	// 	AllowOrigins:     []string{"https://zahiqibrahim.github.io"},
+	// 	AllowMethods:     []string{http.MethodGet, http.MethodOptions},
+	// 	AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: true,
+	// 	MaxAge:           12 * time.Hour,
+	// }))
+	//global middleware
+	router.Use(middleware.CORS(), middleware.RateLimiter())
 
 	router.POST("/add-transaction", middleware.AuthHeaderMiddleware(), controller.AddTransactionController)
 	router.GET("/transactions", controller.GetTransactionController)
